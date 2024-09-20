a565b0e23b3eddc400d1b44444c1842a260c1388
Elias H <69460338+eliazq@users.noreply.github.com>
2024-09-20 13:21:51 +0300

    Slots Position Methods

--- Assets/Scripts/SlotMachine.cs
+++ Assets/Scripts/SlotMachine.cs
@@ -7,19 +7,46 @@ using UnityEngine.UI;
 [ExecuteAlways] // Allows Update to run in the Editor
 public class SlotManager : MonoBehaviour
 {
+    [Space(10f)]
+    [SerializeField] bool editMode = false;
+    [Space(10f)]
     [Range(0.01f, 1f)]
     [SerializeField] float scale = 0.5f;
     [Range(3, 5)]
     [SerializeField] int slots = 3;
 
-    [SerializeField] bool editMode = false;
+    Transform[] verticalSlots; // 5 is max vertical slot count
 
     #region References
     HorizontalLayoutGroup verticalSlotsLayoutGroup;
     Transform verticalSlotsContainer;
     #endregion
 
+    [Header("SlotValues")]
+    float verticalSlotYStart = 1080f; // Position Y Where Slots start spinning down
+    float verticalSlotYEnd = 0f; // Goal Y position;
+
     private bool shouldUpdateSlots = false; // Flag to control updates
+
+    private void Awake()
+    {
+        if (verticalSlotsContainer == null)
+            verticalSlotsContainer = transform.GetChild(0);
+
+        if (verticalSlotsLayoutGroup == null)
+            verticalSlotsLayoutGroup = verticalSlotsContainer.GetComponent<HorizontalLayoutGroup>();
+
+        verticalSlotsLayoutGroup.enabled = false;
+
+        if (editMode == true) Debug.LogError("In Editor Disable Edit Mode In SlotMachine Script When In Play Mode");
+
+        verticalSlots = new Transform[slots];
+
+        for (int i = 0; i < verticalSlotsContainer.childCount; i++)
+        {
+            verticalSlots[i] = verticalSlotsContainer.GetChild(i);
+        }
+    }
 
     private void OnValidate()
     {
@@ -87,9 +114,21 @@ public class SlotManager : MonoBehaviour
     {
         transform.localScale = new Vector3(scale, scale, scale);
     }
-
+    
     private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            ResetSlotPosition();
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            SetSlotPositionsFinish();
+        }
+
+        
+        // EDITOR CODE HERE
+
         // This ensures Update runs even when not in play mode (for Editor use)
         if (Application.isPlaying) return;
 
@@ -97,6 +136,22 @@ public class SlotManager : MonoBehaviour
         {
             UpdateSlots();
             shouldUpdateSlots = false;
+        }
+    }
+
+    private void ResetSlotPosition()
+    {
+        foreach (Transform verticalSlot in verticalSlots)
+        {
+            verticalSlot.localPosition = new Vector3(verticalSlot.localPosition.x, verticalSlotYStart, verticalSlot.localPosition.z);
+        }
+    }
+
+    private void SetSlotPositionsFinish()
+    {
+        foreach (Transform verticalSlot in verticalSlots)
+        {
+            verticalSlot.localPosition = new Vector3(verticalSlot.localPosition.x, verticalSlotYEnd, verticalSlot.localPosition.z);
         }
     }
 }
